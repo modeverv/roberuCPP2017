ea72a6c73cd459ab81ab09345f2465d57c7faddb
modeverv <modeverv@gmail.com>
2017-09-01 00:51:49 +0900

    sec9

--- src/roberuCPP.cpp
+++ src/roberuCPP.cpp
@@ -10,13 +10,14 @@
 using namespace std;
 
 void sec6();
-
+int sec7f(int, int);
 
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	cout << "Super!!!!" << endl;
 	cout << "aaaaa" << endl;
 	sec6();
+	cout << sec7f(10, 11) << endl;
 	return 0;
 }
 
@@ -26,4 +27,7 @@ void sec6() {
 	a++;
 	cout << a << endl;
 	return;
+}
+int sec7f(int x, int y) {
+	return 2 * x + y;
 }
