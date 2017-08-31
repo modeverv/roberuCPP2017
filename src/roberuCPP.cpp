0b9f17698ee57f8eaeae8a9ae3928d4a0420578c
modeverv <modeverv@gmail.com>
2017-09-01 00:48:04 +0900

    sec6

--- src/roberuCPP.cpp
+++ src/roberuCPP.cpp
@@ -9,7 +9,21 @@
 #include <iostream>
 using namespace std;
 
+void sec6();
+
+
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "Super!!!!" << endl;
+	cout << "aaaaa" << endl;
+	sec6();
 	return 0;
+}
+
+void sec6() {
+	int a = 0;
+	a += 2;
+	a++;
+	cout << a << endl;
+	return;
 }
